5c4c2fa0add9cb6b41d04a6c0acb477a00bdfb46
Rajkumar <46551565+Rajpopzein@users.noreply.github.com>
2026-09-23 01:36:23 +0530

    Use hash routing for Render static hosting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import App from './App'
 import {UIProvider} from './ui'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UIProvider>
         <App/>
       </UIProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
